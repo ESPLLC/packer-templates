8bf4945aae4eb4ad9ba50ab4326123b91b40c4c8
Rick Hornsby <richard.hornsby@espus.com>
2018-05-02 11:30:58 -0500

    Set windows credentials dynamically (future support for ec2 amis)

--- cookbooks/packer-templates/recipes/add_postunattend.rb
+++ cookbooks/packer-templates/recipes/add_postunattend.rb
@@ -11,6 +11,10 @@ end
 
 template 'C:/Windows/Panther/Unattend/unattend.xml' do
   source 'postunattend.xml.erb'
+  variables(
+    username: node['credentials']['vm_username'],
+    password: node['credentials']['vm_password']
+  )
 end
 
 template 'C:/Windows/Panther/Unattend/packer_shutdown.bat' do
